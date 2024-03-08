20ecebb53b5582650ac57eb8db00262ab94ad617
JethroMuluba <jethro.muluba@gmail.com>
2024-03-08 01:23:28 +0100

    creat json server and try o fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
+"use client"
 
-export default function Home() {
+import { useEffect, useState } from "react";
+
+async function getData() {
+  const res = await fetch('http://localhost:3000/backgrounds')
+
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+
+
+export default async function Home() {
+  const data = await getData()
+  // console.log(data[0].bgHdrHheroTitle);
+
   return (
     <div className="max-w-full ">
       <main className="flex min-h-screen flex-col  justify-between p-5">
         <div className=" py-5 px-20">
+          
+
+
           <h2> je suis main</h2>
+      
+          
         </div>
 
       </main>
